8894414c3cb47434b80782ef55c7e2374f93c2fd
peter <petergich80@gmail.com>
2024-03-24 22:23:21 +0300

    sell

--- digitalapp/urls.py
+++ digitalapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path("",views.home,name="home"),
-    path('mpesa/callback/', query_stk_status,name="status"),
+    path('mpesa/callback/', views.query_stk_status,name="status"),
     path('accesstoken/', views.get_access_token, name='get_access_token'),
     path('query/', views.query_stk_status, name='query_stk_status'),
     path('sellerlogin',views.sellerlogin,name='sellerlogin'),
